55fb598450f3d69a62dd86b8a7c67aa83698796e
machetis <machetis1989@gmail.com>
2026-02-28 10:29:17 -0500

    feat: www redirect + fix typo

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
         ignoreDuringBuilds: true,
     },
     optimizeFonts: false,
+    async redirects() {
+        return [
+            {
+                source: '/:path*',
+                has: [{ type: 'host', value: 'auto-14.ru' }],
+                destination: 'https://www.auto-14.ru/:path*',
+                permanent: true,
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
