f923d691563f640eb11d78ea666604ea175fcd7d
machetis <machetis1989@gmail.com>
2026-04-28 18:33:10 +0300

    fix: redirect /uslugi to homepage (404 -> 301)

--- next.config.js
+++ next.config.js
@@ -15,6 +15,11 @@ const nextConfig = {
                 destination: 'https://www.auto-14.ru/:path*',
                 permanent: true,
             },
+            {
+                source: '/uslugi',
+                destination: '/',
+                permanent: true,
+            },
         ];
     },
     async headers() {
